8434b240862315de1eeeb4e47249f3b66f2daf1a
Ryan Richardson <rrichardson@ryans-macbook.local>
2013-05-04 15:23:08 -0700

    Initialized Test Hand to a 'Dealed' Hand

--- test.js
+++ test.js
@@ -11,10 +11,18 @@ var deck = [2,3,3,6,5,2,3,5,1,7,5,3,2,6,7,8,4,4,3,12,3,5,3,3,2,1,6];
 
 console.log('original deck = ' + deck);
 console.log('distributed deck... returned[0] = hands, returned[1] = remaining cards.');
-console.log(card.Deal(5,deck));
+
+var start = card.Deal(5,deck);
+
+console.log(start);
 
 console.log('*****TESTING PLAYER OBJECT*****');
-firstPlayer.hand.push(3,4,5,3);
+//firstPlayer.hand.push(start[0][0]);
+for (i=0;i<start[0][0].length;i++)
+    {
+	firstPlayer.hand.push(start[0][0][i])
+    }
+
 console.log('player\'s hand initialized to ' + firstPlayer.hand);
 
 console.log('flip() function called... player played ' + firstPlayer.flip());
